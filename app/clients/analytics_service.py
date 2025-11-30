bdaa29f9015f6f741e542ec6f0cd5565035fe318
caue <cauepaivalira@outlook.com>
2025-11-30 01:42:33 -0300

    Feat: Finished integration with analytics service

--- app/clients/analytics_service.py
+++ app/clients/analytics_service.py
@@ -7,9 +7,9 @@ _URL_ENV_VAR  = os.getenv("ANALYTICS_SERVICE_URL")
 ANALYTICS_SERVICE_URL = _URL_ENV_VAR if _URL_ENV_VAR is not None else ""
 
 _ENDPOINT_ENV_VAR = os.getenv("ANALYTICS_SERVICE_ENDPOINT")
-ANALYTICS_SERVICE_ENDPOINT  = _ENDPOINT_ENV_VAR if _ENDPOINT_ENV_VAR is not None else "/"
+ANALYTICS_SERVICE_ENDPOINT  = _ENDPOINT_ENV_VAR if _ENDPOINT_ENV_VAR is not None else "/analises"
 
-_TIMEOUT = 5
+_TIMEOUT = 20
 logger = logging.getLogger(__name__)
 
 
@@ -18,20 +18,16 @@ async def send_analytics_payload(collector: AnalyticsCollector)->None:
     Best effort, fire-and-forget call to the analytics service endpoint to collect the results
     from a fact-checking pipeline run
     """
-
-    with open("json_dump_analytics.json","w", encoding="utf-8") as f:
-        f.write(collector.to_json())
-
-
     try:
+        full_path = ANALYTICS_SERVICE_URL + ANALYTICS_SERVICE_ENDPOINT
         async with httpx.AsyncClient(timeout=_TIMEOUT) as client:
             resp = await client.post(
-                ANALYTICS_SERVICE_URL,
+                full_path,
                 json=collector.to_dict(),
             )
             logger.info("Analytics status: %s", resp.status_code)
     except Exception:
         pass
         # swallow or log only
-        #logger.exception("Failed to send analytics payload")
+        logger.exception("Failed to send analytics payload")
     
